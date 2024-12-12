729bfd8c74bc0a0a39db150b6accda8a93df6678
SweetWort <will.c.wells.2@gmail.com>
2024-12-12 07:40:53 -0600

    some more testing

--- src/test_script.py
+++ src/test_script.py
@@ -80,7 +80,7 @@ def evaluate_decider(test_data_path):
 
 
 
-fall_data_path = p.Path(__file__).parents[1] / "Fall_Data" / "RawData" / "nov13" / "IQpickles"
+fall_data_path = p.Path(__file__).parents[1] / "Fall_Data" / "RawData" / "dec10" / "IQpickles"
 
 evaluate_decider(fall_data_path)
 
